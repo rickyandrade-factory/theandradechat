0102326859ee05467015ba971a1b3e4697720a6d
nitishtalwar <nitish.talwar@izap.in>
2019-07-26 19:33:55 +0530

    working in dashboard

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
