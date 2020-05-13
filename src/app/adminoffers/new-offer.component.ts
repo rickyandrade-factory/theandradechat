e7d15b2c5ed0c03ca459808b85e8a76b6a0a096d
Mohit Kumar <mohit@izap.in>
2020-05-13 03:51:40 -0700

    new subscription to add new offer has been done

--- src/app/adminoffers/new-offer.component.ts
+++ src/app/adminoffers/new-offer.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
       <button  mat-dialog-close  class="close">
             <span aria-hidden="true">×</span>
       </button> 
-         Add Subscription
+         Add New Offer
       </h4>
    </div>
    <form class="">
@@ -16,71 +16,44 @@ import { Component } from '@angular/core';
         <div class="row">
           <div class="col-xs-12">
             <mat-form-field>
-                <input matInput placeholder="Full Name">
+            <mat-label>User</mat-label>
+                <input matInput placeholder="Select Or Search">
             </mat-form-field>
           </div>
         </div>
-        <div class="row">    
-            <div class="col-sm-6">
-               <mat-form-field>
-                  <input matInput placeholder="Email">
-               </mat-form-field>
-            </div>
-            <div class="col-sm-6">
-               <div class="checkbox">
-                  <label>
-                  <input type="checkbox" class=""> Mark email as verified
-                  </label>
-               </div>
-            </div>
-         </div>
-         <div class="row">
-            <div class="col-sm-6">
-               <mat-form-field>
-                  <input matInput placeholder="Username">
-               </mat-form-field>
-            </div>
-            <div class="col-sm-6">
-               <div class="role">
-                  <label>
-                  Role
-                  </label>
-                  <button class="btn btn-xs btn-link btn-no-shadow" type="button" uib-popover-template="'users/_common/user-type-info.html'" popover-trigger="'mouseenter'" popover-class="popover-lg">
-                  <span class="fa fa-info-circle fa-fw text-muted"></span>
-                  </button>
-               </div>
-               <mat-form-field>
-                  <mat-select placeholder="Select">
-                     <mat-option value="option">Option</mat-option>
-                  </mat-select>
-               </mat-form-field>
-            </div>
-         </div>
-         <div class="row">
-            <div class="col-sm-6">
-                  <mat-form-field>
-                      <input matInput placeholder="Password">
-                  </mat-form-field>
-            </div>
-            <div class="col-sm-6">
-                  <mat-form-field>
-                      <input matInput placeholder="Repeat Password">
-                  </mat-form-field>
-            </div>
-         </div>
-         <div class="row">
-         <div class="col-sm-6">
-            <div class="checkbox">
-               <label>
-               <input type="checkbox" class=""> Send a welcome email
-               </label>
-            </div>
-          </div>  
-         </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <mat-form-field>
+               <mat-label>Chatroom</mat-label>
+               <mat-select placeholder="Select">
+                  <mat-option value="option1">On Point FX Signals 🇺🇸 (USA)</mat-option>
+                  <mat-option value="option1">1-on-1 Training</mat-option>
+                  <mat-option value="option1">Market Analysis</mat-option>
+                  <mat-option value="option1">The Golden Circle</mat-option>
+                  <mat-option value="option1">Präzise FX Signals 🇩🇪 (GER)</mat-option>
+                  <mat-option value="option1">chat dev</mat-option>
+                  <mat-option>7 Day Intense Private Training</mat-option>
+                  <mat-option>Website Testing A/B</mat-option>
+               </mat-select>
+            </mat-form-field>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-xs-12">
+            <mat-form-field>
+               <mat-label>Billing</mat-label>
+               <mat-select placeholder="Select">
+                  <mat-option value="Select">Select</mat-option>
+               </mat-select>
+            </mat-form-field>
+          </div>
+        </div>
       </div>
       <div class="modal-footer wrap-div">
          <button mat-dialog-close class="btn btn-default pull-left" type="button">Cancel</button>
-         <button class="btn btn-blue">Create</button>
+         <button class="btn btn-blue">Save</button>
       </div>
    </form>
 </div>
