3f111240f2262d3407605e3a49a535e1abeb3b0d
Mohit Kumar <mohit@izap.in>
2020-06-01 03:40:45 -0700

    save button has been added to registration settings and avatar edi t has been set at dashboard profile

--- src/app/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from 'angular-web-storage';
 import { NewAvatarService } from '../adminsettings/new-avatar.service';
+import { MatDialog } from '@angular/material';
+import { NewTeamAvatarDialog } from '../adminsettings/adminsettings.component';
 
 @Component({
   selector: 'app-admin-sidebar',
@@ -9,9 +11,15 @@ import { NewAvatarService } from '../adminsettings/new-avatar.service';
 })
 export class AdminSidebarComponent implements OnInit {
   adminImgPath;
-  constructor( private localStorage: LocalStorageService, private newAvatarService: NewAvatarService) {
+  constructor( private localStorage: LocalStorageService,
+    private dialog: MatDialog,
+     private newAvatarService: NewAvatarService) {
     this.adminImgPath= this.localStorage.get('admin_user_profile');
    }
+
+   openteamavatar() {
+    const fileNameDialogRef = this.dialog.open(NewTeamAvatarDialog);
+  }
 
   ngOnInit() {
     this.newAvatarService.newAvatar.subscribe(
