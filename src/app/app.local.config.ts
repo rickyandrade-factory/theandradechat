9607591e09df254acffa0287a1b408e4d015dcac
Puneet Sethi <puneetsethi25@gmail.com>
2020-06-18 16:54:03 +0530

    updated apiurl fro postres

--- src/app/app.local.config.ts
+++ src/app/app.local.config.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable, SystemJsNgModuleLoader } from '@angular/core';
+import { PathLocationStrategy } from '@angular/common';
+import { HttpResponseBase } from '@angular/common/http';
+import { FXCrossRatesDialog } from './dashboard/dashboard.component';
+import { UserService } from './services/user.service';
+import { Reference } from '@angular/compiler/src/render3/r3_ast';
+import { exitCode } from 'process';
 @Injectable()
 
 export class AppLocalConfig {
   config = {
-    apiUrl: 'http://dev.api.ricky.24x7.cloud/',
+    apiUrl: 'http://dev.api.ricky.24x7.cloud/api/v1/',
     nodeServerUrl: 'http://dev.api.ricky.24x7.cloud/',
     // apiUrl: 'http://localhost:3007/api/v1/',
     // nodeServerUrl: 'http://localhost:3007/api/v1/',
